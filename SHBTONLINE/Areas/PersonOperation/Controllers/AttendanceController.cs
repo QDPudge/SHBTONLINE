60f38eec72a03c5839556fb3d3d3de3038428974
nahua105 <30335791+nahua105@users.noreply.github.com>
2018-02-02 09:58:28 +0800

    test

---
+++ SHBTONLINE/Areas/PersonOperation/Controllers/AttendanceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SHBTONLINE.Areas.PersonOperation.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        // GET: PersonOperation/Attendance
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SHBTONLINE/Areas/PersonOperation/PersonOperationAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SHBTONLINE.Areas.PersonOperation
+{
+    public class PersonOperationAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "PersonOperation";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "PersonOperation_default",
+                "PersonOperation/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
