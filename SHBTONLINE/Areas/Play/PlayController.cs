62ef8a885c85ce6bc67664b0585030e831a38210
PUDGE <morecreative@live.cn>
2018-03-23 09:15:38 +0800

    开局提交

---
+++ SHBTONLINE/Areas/Play/PlayController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SHBTONLINE.Areas.Play
+{
+    public class PlayController : Controller
+    {
+        // GET: Play/Play
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
